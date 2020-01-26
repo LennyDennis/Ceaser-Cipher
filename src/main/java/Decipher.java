23d21844fcd4b6853e57d433f71c73c4de1f82f7
LennyDennis <lennydennis@gmail.com>
2020-01-26 11:51:49 +0300

    Create a decipher class and its construcor

--- src/main/java/Decipher.java
+++ src/main/java/Decipher.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Decipher {
+    private String mText;
+    private int mKey;
+
+    public Decipher(String text, int key) {
+        this.mText = text;
+        this.mKey = key;
+    }
+
+    public int getKey() {
+        return this.mKey;
+
+    }
+
+    public String getText() {
+        return this.mText;
+    }
+
+
 }
+
