aa32349b60150590d635f85dd9f6ae74c7d05b85
LennyDennis <lennydennis@gmail.com>
2020-01-26 11:41:31 +0300

    Create a Decipher test class

--- src/test/java/DecipherTest.java
+++ src/test/java/DecipherTest.java
@@ -1,2 +1,13 @@
-class DecipherTest extends GroovyTestCase {
+import org.junit.*;
+import static  org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class DecipherTest {
+    @Test
+    public void theText_shouldBeTheText_hello(){
+        Decipher textInput = new Decipher("jgnnq",2);
+        assertEquals("jgnnq" ,textInput.getText());
+    }
+
 }
+
