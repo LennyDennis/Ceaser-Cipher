35c0083ed8abedb2710e23d3d9c7291c1a0e87b5
LennyDennis <lennydennis@gmail.com>
2020-01-26 11:54:01 +0300

    Pass the decipher text test

--- src/main/java/Decipher.java
+++ src/main/java/Decipher.java
@@ -16,6 +16,39 @@ public class Decipher {
         return this.mText;
     }
 
+    public String decryptText() {
+        if (mKey > 26) {
+            mKey = mKey % 26;
+        } else if (mKey < 0) {
+            mKey = (mKey % 26) + 26;
+        } else {
+            mKey = mKey;
+        }
+        String decryptedText = "";
+        for(int i=0;i<mText.length();i++){
+            char initialChar = mText.charAt(i);
+            if(Character.isLetter(initialChar )){
+                if(Character.isLowerCase(initialChar)){
+                    char newChar = (char)(initialChar-mKey);
+                    if(newChar>'a'){
+                        decryptedText +=  (char)(initialChar+(26-mKey));
+                    }else{
+                        decryptedText += newChar;
+                    }
+                }else{
+                    char newChar = (char)(initialChar+mKey);
+                    if(newChar>'A'){
+                        decryptedText +=  (char)(initialChar+(26-mKey));
+                    }else{
+                        decryptedText += newChar;
+                    }
+                }
+            }else{
+                decryptedText += initialChar;
+            }
+        }
 
+        return null;
+    }
 }
 
